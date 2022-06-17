2232a6c0a95510040c1b22b3bac429bb40361a59
MastukiMoe <matsuki.sousisu@gmail.com>
2022-06-18 02:51:31 +0900

    test script

---
+++ conf.py
@@ -0,0 +1,13 @@
+import os.path as osp
+
+pose_config = 'mmpose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrnet_w48_coco_256x192.py'
+pose_checkpoint = 'https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth'
+det_config = 'mmpose/demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py'
+det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+
+dirs = "test_data"
+
+log_dir_name = "log"
+img_log = osp.join(log_dir_name, "img")
+json_log = osp.join(log_dir_name, "json")
+

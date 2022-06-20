8bac9d23577d52e3deff802f7092007b57d8acf4
MoeMatsuki <matsuki.sousisu@gmail.com>
2022-06-21 02:15:05 +0900

    Update conf.py

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@ pose_checkpoint = 'https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w4
 det_config = 'mmpose/demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py'
 det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 
-dirs = "test_data"
+dirs = "mmpose/tests/data/coco/"
 
 log_dir_name = "log"
 img_log = osp.join(log_dir_name, "img")
